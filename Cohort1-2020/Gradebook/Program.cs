ba33e9df1141b5a7d69e1d2827daba2c94ba8e91
mercesolis <mersolis@live.com>
2020-04-18 00:02:28 -0500

    manymethods

---
+++ Cohort1-2020/Gradebook/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+            string answer = string.Empty;
+            Dictionary<string, string> gradeBook = new Dictionary<string, string>();
+
+
+            do
+            {
+                Console.WriteLine("Enter a student name.");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter the student's grades");
+                string grades = Console.ReadLine();
+
+                gradeBook.Add(name, grades);
+
+                Console.WriteLine("Do you want to add another student? Enter quit to stop");
+                answer = Console.ReadLine().ToLower();
+
+            } while (!answer.Equals("quit"));
+
+            int lowestGrade = 0;
+            int highestGrade = 0;
+            double average = 0.00;
+            foreach (var item in gradeBook)
+            {
+                Console.WriteLine($"{item.Key}\n");
+
+                try
+                {
+                    int[] singleGrades = Array.ConvertAll<string, int>(gradeBook[item.Key].Split(), Convert.ToInt32);
+                    lowestGrade = singleGrades.Min();
+                    highestGrade = singleGrades.Max();
+                    average = singleGrades.Average();
+
+                }
+                catch
+                {
+                    throw;
+                }
+
+                Console.WriteLine($"Highest grade = {highestGrade} Lowest grade = {lowestGrade} Average = {average}");
+            } 
+
+        }
+    }
+}
--- Cohort1-2020/ManyMethods/Program.cs
+++ Cohort1-2020/ManyMethods/Program.cs
@@ -4,28 +4,35 @@ namespace ManyMethods
 {
     public class Program
     {
+        private static object dateOfBirth;
+
         static void Main(string[] args)
         {
             Hello();
             Addition();
             catDog();
-
+            oddEvent();
+            echo();
+            killGrams();
+            date();
+            age();
+            guess();
 
         }
 
         public static void Hello()
         {
             Console.WriteLine("Say Hello");
-            Console.Write("Enter your name: ");
+            Console.WriteLine("Enter your name: ");
             var name = Console.ReadLine();
             Console.WriteLine("Bye, {0}!", name);
         }
 
         public static void Addition()
         {
-            Console.WriteLine("Enter your first number.");
+            Console.WriteLine("Enter your first number:");
             string answer = Console.ReadLine();
-            Console.WriteLine("Enter your second number.");
+            Console.WriteLine("Enter your second number:");
             string secondAnswer = Console.ReadLine();
 
             int first = Convert.ToInt32(answer);
@@ -35,14 +42,111 @@ namespace ManyMethods
             Console.WriteLine("The sum is " + Convert.ToInt32(answer) + Convert.ToInt32(secondAnswer));
 
 
-        
+
         }
 
         public static void catDog()
         {
-            Console.WriteLine("Cats or dogs?");
-            string answer = Console.ReadLine();
-            Console.WriteLine("");
+            Console.WriteLine("Cats or Dogs?");
+            string cats = Console.ReadLine();
+            Console.WriteLine("Meow");
+            string dogs = Console.ReadLine();
+            Console.WriteLine("Woof");
+
+
         }
+
+
+        public static void oddEvent()
+        {
+            int i;
+            Console.WriteLine("Type a Number: ");
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Even number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+                Console.ReadLine();
+
+            }
+        }
+
+
+
+
+
+
+        // A method that ask the user for a word then prints it 3 times, first in all caps, then 2 more time in all lower case
+        static void echo()
+        {
+            Console.WriteLine("Type a word: ");
+            string echo = Console.ReadLine();
+            string ucaseEcho = echo.ToUpper();
+            string lcaseEcho = echo.ToLower();
+            Console.WriteLine(ucaseEcho);
+            Console.WriteLine(lcaseEcho);
+            Console.WriteLine(lcaseEcho);
+
+
+
+        }
+
+
+
+        // A method that ask the user for a weight in pounds, then converts it to killograms
+        static void killGrams()
+        {
+            Console.WriteLine("Enter a weight in pounds: ");
+            double pounds = Convert.ToDouble(Console.ReadLine());
+            double kilograms = pounds / 2.20462262185;
+            Console.WriteLine(pounds + " pounds is " + kilograms + " kilograms");
+            Console.ReadLine();
+        }
+
+
+        // A method that prints out the current date
+        static void date()
+        {
+            string date = DateTime.UtcNow.GetDateTimeFormats('d')[0];
+            Console.WriteLine("The current date is {0}", date);
+        }
+
+
+        // A method that asks the user their birth year, and print out their age
+        static void age()
+        {
+            Console.WriteLine("Enter your birthday dd/mm/yyyy: ");
+            string bday = Console.ReadLine().ToString();
+            int age = DateTime.Today.Year - (DateTime.Parse(bday)).Year;
+            Console.WriteLine("You are {0} years old", age);
+        }
+
+
+
+
+        // A method that ask the user to guess a word, and print out 'CORRECT!!' if the word is "chsarp", otherwise prints out 'WRONG!!'
+        static void guess()
+        {
+            string Input = "";
+            Console.WriteLine("Guess a word: ");
+            Input = Console.ReadLine();
+
+            if (Input == "csharp")
+            {
+                Console.WriteLine("CORRECT!!");
+            }
+            else
+            {
+                Console.WriteLine("WRONG!!");
+            }
+            
+        }
+
+
+
     }
 }
