7741284d9d601fd59be7d954d121c590d8f5afc9
mercesolis <mersolis@live.com>
2020-04-28 09:24:42 -0500

    towers

--- Cohort1-2020/TowersOfHanoi/Program.cs
+++ Cohort1-2020/TowersOfHanoi/Program.cs
@@ -2,27 +2,93 @@
 using System.Collections.Generic;
 
 namespace TowersOfHanoi
-{ 
+{
 
     class Program
     {
-        private static Dictionary<string, Stack<int>> board = new Dictionary<string, Stack<int>>();
+        private static Dictionary<String, Stack<int>> board = new Dictionary<string, Stack<int>>();
+
+
         static void Main(string[] args)
         {
+
             Stack<int> stack = new Stack<int>();
             stack.Push(4);
             stack.Push(3);
             stack.Push(2);
             stack.Push(1);
-            // board will contain the pegs 'ABC' and the disks 'stack'
             board.Add("A", stack);
-            // empty stacks because 'A' will contain all the disks initially
             board.Add("B", new Stack<int>());
             board.Add("C", new Stack<int>());
 
+            do
+            {
+                Console.Clear();
+                PrintBoard();
+                Console.WriteLine("Enter the peg you want to move from:");
+                string from = Console.ReadLine().ToUpper();
+                Console.WriteLine("Enter the peg you want to move to:");
+                string to = Console.ReadLine().ToUpper();
+
+                if (IsMoveValid(from, to))
+                {
+                    stack.Push(stack.Pop());
+                }
+
+                else
+                {
+                    Console.WriteLine("Illegal move.");
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                }
+            } while (!CheckWin());
+
+            Console.Clear();
+            PrintBoard();
+            Console.WriteLine("You Won!");
+            Console.ReadKey();
+
+
+        }
+
+        private static bool IsMoveValid(string from, string to)
+        {
+            if (board[from].Count != 0 && (board[to].Count == 0 || board[from].Peek() < board[to].Peek()))
+            {
+                return true;
+            }
+            
+
+            return false;
+
+
+
+        }
+
+        private static bool CheckWin()
+        {
+            if (board["C"].Count = 4)
+            {
+                return true;
+            }
+            return false;
+
+
+        }
+
+        private static void PrintBoard()
+        {
+            foreach (var item in board)
+            {
+                Console.WriteLine($"\n{item.Key}: ");
+                var numbers = item.Value.ToArray();
+                for (int i = numbers.Length; i > 0; i--)
+                {
+                    Console.WriteLine(numbers[i - 1] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
-
-
 }
 
