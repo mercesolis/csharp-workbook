1a0c246e997d9a9e5d972c257432ac71f68bdd13
mercesolis <mersolis@live.com>
2020-05-18 10:03:27 -0500

    piglatin with comments

--- Cohort1-2020/PigLatin/Program.cs
+++ Cohort1-2020/PigLatin/Program.cs
@@ -2,11 +2,46 @@
 
 namespace PigLatin
 {
-    class MainClass
+    class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', };
+
+            Console.WriteLine("Enter your word: ");
+            string answer = Console.ReadLine();
+
+            string firstLetter = answer.Substring(0, 1); // the first letter 
+            string lastLetter = answer.Substring(answer.Length - 1, 1); // the last letter 
+            int index = answer.IndexOfAny(vowels); // first occurence of a vowel
+            string findFirstVowel = answer.Substring(0, index); //
+
+            string pigLatin = answer.Substring(index) + findFirstVowel; // 
+
+            // The "rules":
+            if (answer.IndexOfAny(vowels) == -1) // no vowels
+            {
+                Console.WriteLine(pigLatin + "ay");
+            }
+            else if (firstLetter.IndexOfAny(vowels) == 0) // first letter is a vowel
+            {
+                if (lastLetter.IndexOfAny(vowels) == 0) // last letter is a vowel
+                {
+                    Console.WriteLine(answer + "yay");
+                }
+                else
+                {
+                    Console.WriteLine(answer + "ay"); 
+                }
+                
+            }
+            else // first letter is a consonant
+            {
+                Console.WriteLine(pigLatin + "ay");
+            }
+            Console.ReadKey();
+            
+            
         }
     }
 }
